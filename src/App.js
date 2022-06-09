8735ff8e48bdd2611e0a7c94337607ef08e4b485
Ruchigevariya <gevariyaruchita@gmail.com>
2022-06-09 21:51:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/header/Header';
-import Home from './Container/Home';
 import Footer from './Components/footer/Footer';
+import Home from './Container/home/Home';
 
 
 function App() {
