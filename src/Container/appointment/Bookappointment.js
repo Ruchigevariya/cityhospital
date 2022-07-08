a2b2fb734578b73feb17ce960fcc9f7f8127e76d
Ruchigevariya <gevariyaruchita@gmail.com>
2022-07-08 15:18:42 +0530

    changes

--- src/Container/appointment/Bookappointment.js
+++ src/Container/appointment/Bookappointment.js
@@ -1,33 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as yup from 'yup';
 import { Formik, Form, useFormik } from 'formik';
 import { NavLink, useHistory } from 'react-router-dom';
 
 function Bookappointment(props) {
+    const [update, setUpdate] = useState(false)
 
     const history = useHistory();
+    useEffect(() => {
+        console.log(props.location.state);
+        let localData =JSON.parse( localStorage.getItem("bookappointment"))
+        if (props.location.state && localData !== null) {
+            let fData = localData.filter((l) => l.id === props.location.state.id)
+            console.log(fData[0]);
+            formikObj.setValues(fData[0])
+            setUpdate(true)
+        }
+
+    }, [])
+
+
+    const handleUpdateData = (values)=>{
+        console.log(values);
+
+        let localData = JSON.parse(localStorage.getItem("bookappointment"));
+
+        let udata = localData.map((l,i) => {
+          if(l.id === values.id){
+            return values;
+          }else{
+            return l;
+          }  
+        })
+
+        localStorage.setItem("bookappointment",JSON.stringify(udata))
+
+        setUpdate(false);
+        formikObj.resetForm();
+        history.replace();
+        history.push("/listappointment")
+    }
 
     let handleInsert = (values) => {
         console.log(values);
 
         const id = Math.floor(Math.random() * 1000)
-        
+
         let data = {
-            id : id,
+            id: id,
             ...values
         }
+
         console.log(data);
 
         let localData = JSON.parse(localStorage.getItem("bookappointment"));
 
-        if(localData === null){
-            localStorage.setItem("bookappointment",JSON.stringify([data]));
-        }else{
+        if (localData === null) {
+            localStorage.setItem("bookappointment", JSON.stringify([data]));
+        } else {
             localData.push(data)
-            localStorage.setItem("bookappointment",JSON.stringify(localData));
+            localStorage.setItem("bookappointment", JSON.stringify(localData));
         }
-        
+        history.push("/listappointment")
+
     }
+
     let schema = yup.object().shape({
         name: yup.string().required("please enter your name."),
         email: yup.string().required("please enter email id").email("please enter valid email id."),
@@ -48,13 +85,17 @@ function Bookappointment(props) {
         },
         validationSchema: schema,
         onSubmit: values => {
-            handleInsert(values)
-            history.push("/listappointment")
+            if(update){
+                handleUpdateData(values)
+            }else{
+
+                handleInsert(values)
+            }
             // alert(JSON.stringify(values, null, 2));
         },
     });
 
-    const { handleChange, errors, handleSubmit, handleBlur, touched } = formikObj;
+    const { handleChange, errors, handleSubmit, handleBlur, touched, values } = formikObj;
 
     return (
         <div>
@@ -77,6 +118,7 @@ function Bookappointment(props) {
                                 <div className="row">
                                     <div className="col-md-4 form-group">
                                         <input
+                                            value={values.name}
                                             type="text"
                                             name="name"
                                             className="form-control"
@@ -89,6 +131,7 @@ function Bookappointment(props) {
                                     </div>
                                     <div className="col-md-4 form-group mt-3 mt-md-0">
                                         <input
+                                            value={values.email}
                                             type="email"
                                             className="form-control"
                                             name="email"
@@ -101,6 +144,7 @@ function Bookappointment(props) {
                                     </div>
                                     <div className="col-md-4 form-group mt-3 mt-md-0">
                                         <input
+                                            value={values.phone}
                                             type="tel"
                                             className="form-control"
                                             name="phone" id="phone"
@@ -114,6 +158,7 @@ function Bookappointment(props) {
                                 <div className="row">
                                     <div className="col-md-4 form-group mt-3">
                                         <input type="date"
+                                            value={values.date}
                                             name="date"
                                             className="form-control datepicker"
                                             id="date"
@@ -124,7 +169,8 @@ function Bookappointment(props) {
                                         {errors.date && touched.date ? <p>{errors.date}</p> : ''}
                                     </div>
                                     <div className="col-md-4 form-group mt-3">
-                                        <select name="department" id="department" className="form-select" onChange={handleChange} onBlur={handleBlur}>
+                                        <select name="department" id="department" className="form-select" onChange={handleChange} onBlur={handleBlur} value={values.department}
+                                        >
                                             <option value>Select Department</option>
                                             <option value="Department 1">Department 1</option>
                                             <option value="Department 2">Department 2</option>
@@ -135,6 +181,7 @@ function Bookappointment(props) {
                                 </div>
                                 <div className="form-group mt-3">
                                     <textarea
+                                        value={values.message}
                                         className="form-control"
                                         name="message"
                                         rows={5}
@@ -150,7 +197,14 @@ function Bookappointment(props) {
                                     <div className="error-message" />
                                     <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
                                 </div>
-                                <div className="text-center"><button type="submit">Make an Appointment</button></div>
+                                <div className="text-center">
+                                    {
+                                        update ?
+                                            <button type="submit">update an Appointment</button>
+                                            : <button type="submit">Make an Appointment</button>
+                                    }
+
+                                </div>
                             </Form>
                         </Formik>
                     </div>
