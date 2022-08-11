59a5e9b996c350123285023c0a455daac8a78aeb
Ruchigevariya <gevariyaruchita@gmail.com>
2022-08-11 18:52:40 +0530

    changes

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -28,7 +28,7 @@ function Header(props) {
                     </div>
                 </div>
                 <header id="header" className="fixed-top">
-                    <div className={`container d-flex align-items-center ${value.theme}`}>
+                    <div className="container d-flex align-items-center">
                         <div className="logo">
                             <NavLink to="/">Home</NavLink>
                             <h1 className="logo me-auto">City</h1><br />
