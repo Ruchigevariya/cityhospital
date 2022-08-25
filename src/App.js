3917d27cd8d018be265025cb58d15297f71497ac
Ruchigevariya <gevariyaruchita@gmail.com>
2022-08-25 17:39:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,43 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Header from './Components/header/Header';
+// import Footer from './Components/footer/Footer';
+// import Home from './Container/home/Home';
+// import { Route, Switch } from 'react-router-dom';
+// import Department from './Container/Department/Department';
+// import Docters from './Container/Docters/Docters';
+// import Contact from './Container/Contact/Contact';
+// import About from './Container/About/About';
+// import Appointment from './Container/appointment/Appointment';
+// import Login_signup from './Container/Loginsignup/Login_signup';
+// import Medicines from './Container/Medicines/Medicines';
+// import Reference from './Container/Reference/Reference';
+// import ToggleContext from './Context/ThemeContext';
+
+// function App() {
+//   return (
+//     <div>
+//     <ToggleContext>
+//     <Header />
+//     <Switch>
+//       <Route path={"/"}  exact component={Home}></Route>
+//       <Route path={"/department"} exact component={Department}></Route>
+//       <Route path={"/docters"} exact component={Docters}></Route>
+//       <Route path={"/about"} exact component={About}></Route>
+//       <Route path={"/contact"} exact component={Contact}></Route>
+//       <Route path={"/appointment"} exact component={Appointment}></Route>
+//       <Route path="/loginsignup" exact component={Login_signup}></Route>
+//       <Route path={"/medicines"} exact component={Medicines}></Route>
+//       <Route path={"/reference"} exact component={Reference}></Route>
+//     </Switch>
+//     <Footer/>
+//     </ToggleContext>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/header/Header';
@@ -8,30 +48,35 @@ import Department from './Container/Department/Department';
 import Docters from './Container/Docters/Docters';
 import Contact from './Container/Contact/Contact';
 import About from './Container/About/About';
-import Appointment from './Container/appointment/Appointment';
+import Bookappointment from './Container/appointment/Bookappointment';
 import Login_signup from './Container/Loginsignup/Login_signup';
 import Medicines from './Container/Medicines/Medicines';
 import Reference from './Container/Reference/Reference';
+import ListAppointment from './Container/appointment/ListAppointment';
+import PublicRoute from './Route/PublicRoute';
+import PrivateRoute from './Route/PrivateRoute';
 import ToggleContext from './Context/ThemeContext';
 
 function App() {
   return (
     <div>
-    <ToggleContext>
-    <Header />
-    <Switch>
-      <Route path={"/"}  exact component={Home}></Route>
-      <Route path={"/department"} exact component={Department}></Route>
-      <Route path={"/docters"} exact component={Docters}></Route>
-      <Route path={"/about"} exact component={About}></Route>
-      <Route path={"/contact"} exact component={Contact}></Route>
-      <Route path={"/appointment"} exact component={Appointment}></Route>
-      <Route path="/loginsignup" exact component={Login_signup}></Route>
-      <Route path={"/medicines"} exact component={Medicines}></Route>
-      <Route path={"/reference"} exact component={Reference}></Route>
-    </Switch>
-    <Footer/>
-    </ToggleContext>
+      <ToggleContext>
+        <Header />
+        <Switch>
+          <PublicRoute path={"/"} exact component={Home} />
+          <PublicRoute path={"/department"} exact component={Department} />
+          <PublicRoute path={"/docters"} exact component={Docters} />
+          <PublicRoute path={"/about"} exact component={About} />
+          <PublicRoute path={"/contact"} exact component={Contact} />
+          <PublicRoute path={"/loginsignup"} exact restricted={true} component={Login_signup} />
+          <PublicRoute path={"/medicines"} exact component={Medicines} />
+          <PublicRoute path={"/reference"} exact component={Reference} />
+          <PrivateRoute path={"/bookappointment"} exact component={Bookappointment} />
+          <PrivateRoute path={"/listappointment"} exact component={ListAppointment} />
+        </Switch>
+        <Footer />
+      </ToggleContext>
+
     </div>
   );
 }
