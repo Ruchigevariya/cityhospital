d4085cce3f1c8feed3064dfd2ef3f931d6d6dbba
Ruchigevariya <gevariyaruchita@gmail.com>
2022-08-25 17:40:07 +0530

    changes

--- src/Components/Alert/Alert.js
+++ src/Components/Alert/Alert.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function Alert(props) {
-    // const alert = useSelector(state => state.alert)
+    const alert = useSelector(state => state.alert)
     console.log(alert);
     
     return (
