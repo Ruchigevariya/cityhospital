c925374f98d170f00aa03fb0f99df571d3c53db0
Ruchigevariya <gevariyaruchita@gmail.com>
2022-09-02 10:43:02 +0530

    changes

--- src/common/API/auth.api.js
+++ src/common/API/auth.api.js
@@ -76,7 +76,7 @@ export const SignOutApi = () => {
                 resolve({ payload: "LogOut Successful" })
             })
             .catch((error) => {
-                reject({ payload: error.code })
+                reject({ payload: error })
             })
     })
 }
